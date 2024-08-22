7ae55303efda872eee428c8090b028b840c80e83
Taras Byalyk <byalykt@hotmail.com>
2024-08-23 00:25:04 +0200

    text placement fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="justify-centerg flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       Hello, World!
     </main>
   );
