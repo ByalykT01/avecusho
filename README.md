3f364235e014bee655f9a186b83c4f9c0b17198f
Taras Byalyk <byalykt@hotmail.com>
2024-08-23 00:20:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Web Commerce Gallery
+
+## TODO
+
+- [ ] basic deploy
+- [ ] scaffold basic UI
+- [ ] set up DB (vercel postgres)
+- [ ] attach DB to UI
+- [ ] add auth (w/ Clerk)
+- [ ] add image upload
+- [ ] parallel routes (working!!!)
+- [ ] analytics (posthog)
+- [ ] ratelimiting (upstash)
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Avecusho",
+  description: "Genius artist's store",
+  icons: [{ rel: "icon", url: "/avecusho.svg" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
