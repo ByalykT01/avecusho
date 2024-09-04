703f5f855547a163aa0643dc174bb0312337e8e4
Taras Byalyk <byalykt@hotmail.com>
2024-09-04 21:31:43 +0200

    added suspence for login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "~/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function LogInPage() {
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
